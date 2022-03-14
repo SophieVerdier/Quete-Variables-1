4bec1ec121e99bc9ed162604cfec7bf19c4f4f38
Sophie Verdier <100239319+SophieVerdier@users.noreply.github.com>
2022-03-14 20:29:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$filmName = " Indiana Jones and the Last Crusade"; 
+echo $filmName ."<br>".
+
+
+$seen = true; 
+if($seen === true){
+echo "J'ai vu le film";
+}
+else {
+echo "Je n'ai pas vu le film";}
+
+
+$dateSortie = 1989; 
+echo $dateSortie ."<br>".
+
+$note = "8.3"; 
+echo $note;
+
+php/> 
